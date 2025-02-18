6292a83a4c68cd56ca548325aa9deb247e357f52
Elisei <eliseivlas.002@gmail.com>
2025-02-18 16:40:25 +0100

    primo bonus

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,24 +3,48 @@ import { useState } from 'react';
 const marchioMacchine = ["bmw", "mercedes", "audi", "opel", "toyota"]
 // componente
 export default function Main() {
+    // stato della lista
+    const [marchiMacchine, aggiungiMarchio] = useState(marchioMacchine);
 
-    const [firstName, setFirstName] = useState('Marco');
+    // stato del imput che ggiunge marchi di macchine
+    const [nuovoMarchio, setNuovoMarchio] = useState('');
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        console.log('Il nome inviato è:' + firstName)
+    // funzione di CB di aggiunta elemento alla todo
+    const addTask = e => {
+        e.preventDefault();
+        // modifica 
+        const updatedTasks = [...marchiMacchine, nuovoMarchio];
+        aggiungiMarchio(updatedTasks);
     }
-    function stampaLista() {
-        return marchioMacchine.map(lista => {
-            return <li>{lista}</li>
-        }
 
-        )
+       // funzione di CB di cancellazione elemento dalla lista
+        const eliminaMarchio = i => {
+        const updatedTasks = marchiMacchine.filter((task, index) => {
+            return index !== i
+        });
+        aggiungiMarchio(updatedTasks);
     }
+  
     return (
-        <>
-           
-            {stampaLista()}
+        <> 
+         {/* form di gestione lista */}
+        <form onSubmit={addTask}>
+            <input type="text" value={nuovoMarchio}
+                onChange={event => { setNuovoMarchio(event.target.value) }}
+            />
+            <button>Invia</button>
+        </form >
+            
+            {/*output lista  */}
+        {marchiMacchine.map((task, i) => (
+            <li key={i}>
+                {task}
+                {/* bottone di eliminazione elemento */}
+                <button onClick={() => eliminaMarchio(i)}>
+                    Elimina
+                </button>
+            </li>
+         ))}
         </>
     )
 }
