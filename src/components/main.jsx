3b72d111428a041bc18449d5c2eb2e220cab578d
Elisei <eliseivlas.002@gmail.com>
2025-02-18 17:12:42 +0100

    consegna esercizio

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 
-const marchioMacchine = ["bmw", "mercedes", "audi", "opel", "toyota"]
+const marchioMacchine = [
+    { id: 1, title: "Bmw" },
+    { id: 2, title: "Mercedes" },
+    { id: 3, title: "Audi" },
+    { id: 4, title: "Opel" },
+    { id: 5, title: "Toyota" },
+  ];
 // componente
 export default function Main() {
     // stato della lista
@@ -12,19 +18,28 @@ export default function Main() {
     // funzione di CB di aggiunta elemento alla todo
     const addTask = e => {
         e.preventDefault();
+
+        const nuovoElemento = {
+            id: marchiMacchine.length + 1, // Genera un ID incrementale
+            title: nuovoMarchio.charAt(0).toUpperCase() + nuovoMarchio.slice(1), // Capitalizza la prima lettera
+          };
         // modifica 
-        const updatedTasks = [...marchiMacchine, nuovoMarchio];
-        aggiungiMarchio(updatedTasks);
+        aggiungiMarchio([...marchiMacchine, nuovoElemento]);
+        setNuovoMarchio("")
     }
 
        // funzione di CB di cancellazione elemento dalla lista
-        const eliminaMarchio = i => {
-        const updatedTasks = marchiMacchine.filter((task, index) => {
-            return index !== i
+        const eliminaMarchio = (id) => {
+        const updatedTasks = marchiMacchine.filter((task) => {
+            return task.id !== id
         });
         aggiungiMarchio(updatedTasks);
+        
+        
     }
-  
+    console.log(marchiMacchine);
+    
+    
     return (
         <> 
          {/* form di gestione lista */}
@@ -32,15 +47,15 @@ export default function Main() {
             <input type="text" value={nuovoMarchio}
                 onChange={event => { setNuovoMarchio(event.target.value) }}
             />
-            <button>Invia</button>
+            <button type="submit">Invia</button>
         </form >
             
             {/*output lista  */}
-        {marchiMacchine.map((task, i) => (
-            <li key={i}>
-                {task}
+        {marchiMacchine.map((task) => (
+            <li key={task.id}>
+                {task.title}
                 {/* bottone di eliminazione elemento */}
-                <button onClick={() => eliminaMarchio(i)}>
+                <button onClick={() => eliminaMarchio(task.id)}>
                     Elimina
                 </button>
             </li>
